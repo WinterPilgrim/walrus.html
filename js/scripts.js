a5e22c5951c354c7d04d7d7e641b188efadbc133
Karla Buckner and Laura Greene <laura@>
2017-05-22 20:49:19 -0700

    Add images, jQuery functions.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+  $("#Walter").click(function() {
+    alert("Coo Coo Ca-choo.");
+  });
+
+  $(".Joe p").click(function() {
+      $(".Joe img").toggle();
+    });
+
+      $(".Doris p").click(function() {
+          $(".Doris img").toggle();
+        });
+
+          $(".Bob p").click(function() {
+              $(".Bob img").toggle();
+            });
+      });
